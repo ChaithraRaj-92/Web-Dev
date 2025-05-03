2045fd5902d84015f3e103f90ca94a6b803fdd4e
ChaithraRaj-92 <rajchaithra92@gmail.com>
2025-05-03 23:31:29 +0530

    Create Test Script.java

---
+++ Test Script.java
@@ -0,0 +1,10 @@
+public class SumExample {
+    public static void main(String[] args) {
+        int num1 = 10;  // first number
+        int num2 = 20;  // second number
+
+        int sum = num1 + num2;  // calculate sum
+
+        System.out.println("The sum is: " + sum);  // print result
+    }
+}
